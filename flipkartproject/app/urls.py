c31d2261bb59c0238a3ed8d75071789e7fd061e9
kinjaltailor <83589855+kinjaltailor@users.noreply.github.com>
2025-02-27 08:21:19 +0000

    second commit

--- flipkartproject/app/urls.py
+++ flipkartproject/app/urls.py
@@ -15,5 +15,12 @@ urlpatterns = [
     path("reset_password/<uname>/", views.reset_password, name="reset_password"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
+    path("mobilelist/", views.mobilelist, name="mobilelist"),
+    path("clothslist/", views.clothslist, name="clothslist"),
+    path("shoeslist/", views.shoeslist, name="shoeslist"),
+    path("electronicslist/", views.electronicslist, name="electronicslist"),
+    path("showpricerange/", views.showpricerange, name="showpricerange"),
+    path("sortingbyprice/", views.sortingbyprice, name="sortingbyprice"),
+    path("searchproduct/", views.searchproduct, name="searchproduct"),
 
 ]
